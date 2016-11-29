b692733a2ae37e3956d6bb81e845fc7e8db00bfc
Jessica Tran <Jess@Jessica-Trans-MacBook-Air.local>
2016-11-29 22:49:36 +1100

    created loop

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,5 +5,11 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-
-
+//Loop, if cards have the same value then alert "You found a match!" Otherwise alert "Sorry, try again."
+if (cardTwo === cardFour || cardOne === cardThree || cardOne === cardFour || cardTwo === cardThree){
+	alert("Sorry, try again.");
+} else if (cardOne === cardTwo){
+	alert ("You found a match!");
+	else (cardThree === cardFour){
+	alert("You found a match!");
+}
